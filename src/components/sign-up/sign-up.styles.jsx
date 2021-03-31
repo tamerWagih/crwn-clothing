536ca426af18544d9a6863f9156dc987db268c0a
Tamer Wagih <mrtamerwagih@gmail.com>
2021-04-01 01:22:46 +0300

    Adding lazy loading to some imp routes and some small screen responsiveness

--- src/components/sign-up/sign-up.styles.jsx
+++ src/components/sign-up/sign-up.styles.jsx
@@ -4,8 +4,16 @@ export const SignUpContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 380px;
+
+  @media screen and (max-width: 800px) {
+    margin-top: 20px;
+  }
 `;
 
 export const SignUpTitle = styled.h2`
   margin: 10px 0;
+
+  @media screen and (max-width: 800px) {
+    margin-top: 30px;
+  }
 `;
